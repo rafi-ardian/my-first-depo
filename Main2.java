1584b6a025bd9f65a2a15c24800a389e6751cba6
rafi-ardian <rafirappp@gmail.com>
2022-04-04 14:29:26 +0700

    first commit

---
+++ Main2.java
@@ -0,0 +1,17 @@
+package study;
+
+
+public class Main2 {
+    public static void main(String[] args) {
+        Biodata b= new Biodata();
+        
+        b.getFullName("Rafi Ardian Yusuf");
+        b.getPlaceBirth("Jakarta");
+        b.getDateBirth(01," Juli ",2003);
+        b.getHobby("Sleep");
+        b.getZodiac("Cancer");
+        b.getFavFood("Indomie");
+        b.getDreams("Software Engineer");
+        
+    }
+}
