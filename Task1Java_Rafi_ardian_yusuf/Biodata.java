8a970e9cd6a299034c5bc347b8b6cfe8946a4d4e
rafiardian <102891536+rafi-ardian@users.noreply.github.com>
2022-04-04 15:38:50 +0700

    Add files via upload

---
+++ Task1Java_Rafi_ardian_yusuf/Biodata.java
@@ -0,0 +1,30 @@
+package study;
+
+
+public class Biodata {
+    
+    
+    public void getFullName(String fullName){
+        System.out.println("Nama lengkap = "+fullName);
+    }
+    public void getPlaceBirth(String place){
+        System.out.println("Tempat lahir = "+place);
+    }
+    public void getDateBirth(int x,String date,int z){
+        System.out.println("Tanggal lahir = "+x+"-"+date+"-"+z);
+    }
+    public void getHobby(String hobby){
+        System.out.println("Hobi = "+hobby);
+    }
+    public void getZodiac(String Zodiac){
+        System.out.println("Zodiac =  "+Zodiac);
+    }
+    public void getFavFood(String FavFood){
+        System.out.println("Makanan favorit = "+FavFood);
+    }
+    public void getDreams(String Dreams){
+        System.out.println("Cita-cita = "+Dreams);
+    }
+    
+    
+}
