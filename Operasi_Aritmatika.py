63098168eb4470b79772bc3900afb360ce6655fb
rafiardian <102891536+rafi-ardian@users.noreply.github.com>
2022-04-08 22:32:20 +0700

    Add files via upload

---
+++ Operasi_Aritmatika.py
@@ -0,0 +1,21 @@
+#Operasi Aritmatika
+
+
+a = 10
+b = 3
+
+#Sum
+sum = a+b
+print (a, '+',b,'=',sum)
+
+#Sub =
+sub = a-b
+print (a, '-',b,'=',sub)
+
+#Multiply
+multiply = a*b
+print (a, '*',b,'=',multiply)
+
+#Divide
+divide = a/b
+print (a, '/',b,'=',divide)
